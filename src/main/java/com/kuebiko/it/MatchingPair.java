1b3d3be635efac8c61639959ed8b5ff7552bd0c4
kbhatta09 <kushalbhatta7@gmail.com>
2020-03-11 20:48:37 -0700

    q

---
+++ src/main/java/com/kuebiko/it/MatchingPair.java
@@ -0,0 +1,18 @@
+package com.kuebiko.it;
+
+import java.util.List;
+
+/**
+ * Given array and sum find pair/s of number that equals the sum
+ * if none found return empty list
+ *
+ * arr = [1,2,3,4,5] and sum = 5 => [(1,4), (2,3)]
+ * arr = [-1,1,2,3,4,-5] and sum = 3 => [(-1,4), (1,2)]
+ * arr = [1,2,3] and sum = 7 => []
+ */
+public class MatchingPair {
+
+    public List<String> matchPair(int[] arr, int sum) {
+        return null;
+    }
+}
