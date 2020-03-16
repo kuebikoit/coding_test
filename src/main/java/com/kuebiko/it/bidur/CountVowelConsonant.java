3ce5167b6d26aeeecef781c5f83dca9d2e531bd7
Hak-bit <hak.bit010@gmail.com>
2020-03-16 16:19:33 -0500

    StringRemoval

---
+++ src/main/java/com/kuebiko/it/bidur/CountVowelConsonant.java
@@ -0,0 +1,15 @@
+package com.kuebiko.it.bidur;
+/**
+ * Count the number of vowel and consonant in a given string of only alphabets
+ * and return in two element array first contains number of vowels
+ * second the number of consonants
+ * eg. "HelloWorld" => [3, 7]
+ * "KuebikoIt" => [5, 4]
+ */
+public class CountVowelConsonant {
+
+    public int[] countVowelConsonant(String input) {
+        return null;
+    }
+
+}
