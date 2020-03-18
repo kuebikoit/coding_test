50a839b01784884ac1e7cc787ab867eb8c659370
ROSHAN1112 <roshan28880@gmail.com>
2020-03-17 21:06:34 -0700

    roshan shrestha- 7 total files in commit

--- src/main/java/com/kuebiko/it/roshan/StringRemoval.java
+++ src/main/java/com/kuebiko/it/roshan/StringRemoval.java
@@ -11,7 +11,31 @@ package com.kuebiko.it.roshan;
  */
 public class StringRemoval {
 
+
   public String remove(String s1, String s2) {
-    return null;
+
+    /*Lowercase the strings to compare */
+    String s1LowCase=s1.toLowerCase();
+    String s2LowCase=s2.toLowerCase();
+
+    StringBuilder s1B= new StringBuilder(s1);
+    int s2Length = s2.length();
+    int indexHolder = s1LowCase.indexOf(s2LowCase);
+    int endIndexHolder = indexHolder+s2Length;
+    StringBuilder finalString;
+    String finalOutput;
+
+
+          if(indexHolder>=0){
+              finalString= s1B.delete(indexHolder,endIndexHolder);
+              finalOutput = finalString.toString();
+          }
+
+          else{
+              finalOutput="N/A";
+          }
+
+    return finalOutput;
+
   }
 }
