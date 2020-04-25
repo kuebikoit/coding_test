78fb5119a5ceb0c58ed1ea4d3b721d49c416091b
ROSHAN1112 <roshan28880@gmail.com>
2020-04-24 18:42:42 -0700

    2nd test FINAL roshan shrestha

--- src/main/java/com/kuebiko/it/design/farm/Chicken.java
+++ src/main/java/com/kuebiko/it/design/farm/Chicken.java
@@ -1,17 +1,32 @@
 package com.kuebiko.it.design.farm;
 
 import com.kuebiko.it.design.farm.exception.NotYetImplementedException;
-import java.time.Instant;
+import com.opencsv.CSVWriter;
+
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Properties;
+
+//import com.sun.xml.internal.stream.buffer.sax.Properties;
 
 public class Chicken implements Bird {
 
   private final String name;
 
-  //private static final long HATCHING_PERIOD_MINS = initialize();
-  //private static final long HATCHING_PERIOD_MINS = incubation.period.minutes.chicken;
+  private static long HATCHING_PERIOD_MINS;
+
+  static {
+    try {
+      HATCHING_PERIOD_MINS = initialize();
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+  }
+
 
   private List<Egg> eggs = new ArrayList<>(100);
 
@@ -19,16 +34,75 @@ public class Chicken implements Bird {
     this.name = name;
   }
 
-  static long initialize() {
+
+  static long initialize() throws IOException {
+
+    try (FileReader reader = new FileReader("C:\\Users\\Roshan\\Documents\\SPRING 2020\\cubic\\coding_test\\src\\main\\resources\\farm\\bird.properties")) {
+      Properties properties = new Properties();
+      properties.load(reader);
+
+      String hatchingTime = properties.getProperty("incubation.period.minutes.chicken");
+      long finalHatchingTime = Integer.parseInt(hatchingTime);
+      return finalHatchingTime;
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
     throw new NotYetImplementedException("get from src/main/resources/farm/bird.properties");
   }
 
+
   @Override
-  public Egg lay() {
+  public Egg lay() throws IOException {
     System.out.println(String.format("chicken(%s) laid an egg at %s", name, LocalDateTime.now()));
+
+    Egg egg = new Egg(this::incubation);
+    this.eggs.add(egg);
+
+    System.out.println("eggs:" + eggs);
+    System.out.println("egg hatched?: " + egg.isAlreadyHatched);
 
     throw new NotYetImplementedException("add it to eggs after its laid");
 
     //System.out.println(String.format("total egg count is %s", eggs.size()));
   }
+
+
+  private Chicken incubation() {
+    try {
+      Thread.sleep(21*1000 );
+      EggCsv();
+
+    } catch (InterruptedException e) {
+      e.printStackTrace();
+    }
+    return null;
+
+  }
+
+
+  private void EggCsv()  {
+
+    //Write into egg.csv
+    String filepath = "src/main/resources/farm/egg.csv";
+    FileWriter fw = null;
+    try {
+      fw = new FileWriter(filepath,true);
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+    CSVWriter writer = new CSVWriter(fw);
+
+    String[] bird_type = {this.name, String.valueOf(LocalDateTime.now())};
+
+    writer.writeNext(bird_type);
+    try {
+      writer.close();
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+
+
+  }
+
+
 }
--- src/main/java/com/kuebiko/it/design/farm/FarmRunner.java
+++ src/main/java/com/kuebiko/it/design/farm/FarmRunner.java
@@ -6,12 +6,14 @@ import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
 
 public class FarmRunner {
+
   private static final ExecutorService executorService = Executors.newFixedThreadPool(5);
 
   public static void main(String[] args) {
     ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(1);
 
     Bird myChicken = new Chicken("potent hen");
+
 
     scheduler.scheduleAtFixedRate(() -> simulate(myChicken), 2, 10, TimeUnit.SECONDS);
   }
