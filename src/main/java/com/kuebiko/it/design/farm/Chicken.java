0d168ed7ab226fc13c75753400a900a51b56c74e
Sazan245 <don.sazan245@gmail.com>
2020-04-25 12:11:04 -0500

    Submitting Second Test

--- src/main/java/com/kuebiko/it/design/farm/Chicken.java
+++ src/main/java/com/kuebiko/it/design/farm/Chicken.java
@@ -1,33 +1,82 @@
 package com.kuebiko.it.design.farm;
 
+import au.com.bytecode.opencsv.CSVWriter;
 import com.kuebiko.it.design.farm.exception.NotYetImplementedException;
+
+import java.io.*;
 import java.time.Instant;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Properties;
+import java.util.concurrent.*;
 
 public class Chicken implements Bird {
 
-  private final String name;
+    private final String name;
 
-  //private static final long HATCHING_PERIOD_MINS = initialize();
+    private static final long HATCHING_PERIOD_MINS = initialize();
 
-  private List<Egg> eggs = new ArrayList<>(100);
+    private List<Egg> eggs = new ArrayList<>(100);
 
-  public Chicken(String name) {
-    this.name = name;
-  }
+    public Chicken(String name) {
+        this.name = name;
+    }
 
-  static long initialize() {
-    throw new NotYetImplementedException("get from src/main/resources/farm/bird.properties");
-  }
+    static long initialize() {
+        InputStream is;
+        Properties prop = null;
+        try {
+            prop = new Properties();
+            is = new FileInputStream(new File("src/main/resources/farm/bird.properties"));
+            prop.load(is);
 
-  @Override
-  public Egg lay() {
-    System.out.println(String.format("chicken(%s) laid an egg at %s", name, LocalDateTime.now()));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return Long.parseLong(prop.getProperty("incubation.period.minutes.chicken"));
+    }
 
-    throw new NotYetImplementedException("add it to eggs after its laid");
+    @Override
+    public Egg lay() throws Exception {
+        System.out.println(String.format("%s laid an egg at %s", name, LocalDateTime.now()));
 
-    //System.out.println(String.format("total egg count is %s", eggs.size()));
-  }
+        Egg egg = new Egg(this::Onhatching);
+        String time = LocalDateTime.now().toString();
+        System.out.println(String.format("%s hatch an egg at %s", egg.getBird(), time));
+        eggs.add(egg);
+
+
+        //Writing egg type and hatched time to the egg.csv file
+        File file = new File("src/main/resources/farm/egg.csv");
+        CSVWriter writer = new CSVWriter(new FileWriter(file, true));
+        String[] record = {String.valueOf(egg.getBird()),  time};
+        writer.writeNext(record);
+
+        writer.close();
+
+
+
+        return egg;
+
+        //System.out.println(String.format("total egg count is %s", eggs.size()));
+    }
+
+    private Chicken Onhatching(){
+        try {
+            TimeUnit.MINUTES.sleep(HATCHING_PERIOD_MINS);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        return this;
+    }
+
+    @Override
+    public String toString() {
+        return "Chicken{" +
+                "name='" + name + '\'' +
+                '}';
+    }
+
+
 }
--- src/main/java/com/kuebiko/it/design/farm/Egg.java
+++ src/main/java/com/kuebiko/it/design/farm/Egg.java
@@ -1,19 +1,28 @@
 package com.kuebiko.it.design.farm;
 
 import com.kuebiko.it.design.farm.exception.NotYetImplementedException;
-import java.util.concurrent.Callable;
+
+import java.util.concurrent.*;
 
 public final class Egg {
 
   private final Bird bird;
-
   private boolean isAlreadyHatched;
 
-  public Egg(Callable<Bird> birdCallable) {
-    throw new NotYetImplementedException("when timer expires isAlreadyHatched = true");
+
+  public Egg(Callable<Bird> birdCallable) throws Exception {
+    this.bird = birdCallable.call();
+    this.isAlreadyHatched=true;
   }
 
   public Bird getBird() {
     return this.bird;
+  }
+
+  @Override
+  public String toString() {
+    return "Egg{" +
+            "bird=" + bird +
+            '}';
   }
 }
