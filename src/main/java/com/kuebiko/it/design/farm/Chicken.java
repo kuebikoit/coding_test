33e896359fa295058d1d83a18a4c0a0e2ea9b02a
shrums_owen <shrumsowen11@gmail.com>
2022-11-27 18:30:00 -0500

    minor update

--- src/main/java/com/kuebiko/it/design/farm/Chicken.java
+++ src/main/java/com/kuebiko/it/design/farm/Chicken.java
@@ -1,9 +1,10 @@
 package com.kuebiko.it.design.farm;
 
-import com.kuebiko.it.design.farm.exception.NotYetImplementedException;
 import com.opencsv.CSVWriter;
 
 import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.time.Instant;
 import java.time.LocalDateTime;
 import java.util.*;
@@ -18,7 +19,7 @@ public class Chicken implements Bird {
 
     private static final long HATCHING_PERIOD_MINS = initialize();
 
-    private List<Egg> eggs = new ArrayList<>(100);
+    private final List<Egg> eggs = new ArrayList<>(100);
 
 
     public Chicken(String name) {
@@ -29,7 +30,7 @@ public class Chicken implements Bird {
     static long initialize() {
 
         Properties property = new Properties();
-        try (InputStream input = new FileInputStream("C:\\Users\\bhask\\IdeaProjects\\secondTest\\coding_test\\src\\main\\resources\\farm\\bird.properties")) {
+        try (InputStream input = Files.newInputStream(Paths.get("D:\\Projects\\coding_test\\src\\main\\resources\\farm\\bird.properties"))) {
             property = new Properties();
 
             if (input == null) {
@@ -48,7 +49,7 @@ public class Chicken implements Bird {
     //* In chicken class implement method `lay()`, use `HATCHING_PERIOD_MINS` when the egg is laid in a way when that period expires the `isAlreadyHatched` is `true` in `Egg` class
     @Override
     public Egg lay() {
-        System.out.println(String.format("chicken(%s) laid an egg at %s", name, LocalDateTime.now()));
+        System.out.printf("chicken(%s) laid an egg at %s%n", name, LocalDateTime.now());
         Egg egg = null;
         try {
             egg = new Egg(() -> new Chicken(name));
@@ -59,33 +60,33 @@ public class Chicken implements Bird {
         }
         //After egg is laid, first thing put the laid egg inside the ArrayList above and send that egg into a scheduledThreadEgg for hatching after certain time.
         try {
-                ScheduledExecutorService scheduled = Executors.newScheduledThreadPool(1);
-                //makes scheduledTasks--> scheduledTask because, we will have to wait for 21 minutes before doing some task, and so ScheduledFuture returned type.
+            ScheduledExecutorService scheduled = Executors.newScheduledThreadPool(1);
+            //makes scheduledTasks--> scheduledTask because, we will have to wait for 21 minutes before doing some task, and so ScheduledFuture returned type.
 
-                ScheduledFuture<Bird> newBird = scheduled.schedule(() -> new Chicken(name), HATCHING_PERIOD_MINS, MINUTES);
-                //here "()->new Chicken(name)" is a Callable. CallableTask which do not take any input and returns output a ChickenObject along with its name.
-                //We need ChickenObject because a new Chicken is getting born.
+            ScheduledFuture<Bird> newBird = scheduled.schedule(() -> new Chicken(name), HATCHING_PERIOD_MINS, MINUTES);
+            //here "()->new Chicken(name)" is a Callable. CallableTask which do not take any input and returns output a ChickenObject along with its name.
+            //We need ChickenObject because a new Chicken is getting born.
 
 
-                WriteIntoCSV(name.toString(), Instant.now());      //write into the .csv file for the recorded hatched Egg
+            writeIntoCSV();      //write into the .csv file for the recorded hatched Egg
 
-        }catch(Exception e){
-                e.printStackTrace();
-            }
-                //throw new NotYetImplementedException("add it to eggs after its laid");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        //throw new NotYetImplementedException("add it to eggs after its laid");
 
-                //For this, user have to "add" eggs to the List<Egg> made above, --> for this, we need egg variable which can be added done above ↑.
+        //For this, user have to "add" eggs to the List<Egg> made above, --> for this, we need egg variable which can be added done above ↑.
 
-                System.out.println(String.format("total egg count is %s", eggs.size()));
-      return egg;
+        System.out.printf("total egg count is %s%n", eggs.size());
+        return egg;
     }
 
     ////A fileWriter needs to be created so that we can write the updates back to the "egg.csv" file
-    public void WriteIntoCSV(String chickenName, Instant it) {
-        try (FileWriter fileWriter = new FileWriter("C:\\Users\\bhask\\IdeaProjects\\secondTest\\coding_test\\src\\main\\resources\\farm\\egg.csv", true);
+    public void writeIntoCSV() {
+        try (FileWriter fileWriter = new FileWriter("D:\\Projects\\coding_test\\src\\main\\resources\\farm\\egg.csv", true);
              CSVWriter writer = new CSVWriter(fileWriter)) {
             String[] eggEntry = (String[]) eggs.toArray();
-                writer.writeNext(eggEntry);
+            writer.writeNext(eggEntry);
 
         } catch (IOException e) {
             e.printStackTrace();
--- src/main/java/com/kuebiko/it/design/farm/Egg.java
+++ src/main/java/com/kuebiko/it/design/farm/Egg.java
@@ -1,33 +1,31 @@
 package com.kuebiko.it.design.farm;
 
-import com.kuebiko.it.design.farm.exception.NotYetImplementedException;
-
 import java.util.concurrent.*;
 
 public final class Egg {
 
-  private final Bird bird;
+    private final Bird bird;
 
-  private boolean isAlreadyHatched;
+    private boolean isAlreadyHatched;
 
-  int numberOfEggsHatched = 0;
+    int numberOfEggsHatched = 0;
 
-  public Egg(Callable<Bird> birdCallable) throws InterruptedException, ExecutionException {
-    Thread.sleep(21*1000);
+    public Egg(Callable<Bird> birdCallable) throws InterruptedException, ExecutionException {
+        Thread.sleep(21 * 1000);
 
-    ScheduledExecutorService scheduled = Executors.newScheduledThreadPool(1);
-    Future<Bird> newBornBird = scheduled.submit(birdCallable);
-    isAlreadyHatched = true;
+        ScheduledExecutorService scheduled = Executors.newScheduledThreadPool(1);
+        Future<Bird> newBornBird = scheduled.submit(birdCallable);
+        isAlreadyHatched = true;
 
-    if (isAlreadyHatched == true){
-      System.out.println(numberOfEggsHatched+1 +" egg/s hatched...");
+        if (isAlreadyHatched) {
+            System.out.println(numberOfEggsHatched + 1 + " egg/s hatched...");
+        }
+        isAlreadyHatched = false;
+        bird = newBornBird.get();
+        //throw new NotYetImplementedException("when timer expires isAlreadyHatched = true");
     }
-    isAlreadyHatched =false;
-    bird = newBornBird.get();
-    //throw new NotYetImplementedException("when timer expires isAlreadyHatched = true");
-  }
 
-  public Bird getBird() {
-    return this.bird;
-  }
+    public Bird getBird() {
+        return this.bird;
+    }
 }
